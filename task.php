2c40016e7c6612137518d618a4eac076ccf3e648
kaitoiha <kaitooooo18@gmail.com>
2021-10-18 21:40:56 +0900

    q15-q16

--- task.php
+++ task.php
@@ -165,25 +165,39 @@ foreach ($data as $one_data){
 print_r($new_data);
 echo PHP_EOL;
 
-// print("#####q15#####".PHP_EOL);
-// $data1 = ["name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin"];
-// $data2 = ["name" => "yamada", "hobby" => "baseball", "role" => "normal"];
+print("#####q15#####".PHP_EOL);
+$data1 = ["name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin"];
+$data2 = ["name" => "yamada", "hobby" => "baseball", "role" => "normal"];
 
-// // 以下に回答を記載
+// 以下に回答を記載
+if(array_key_exists("age",$data1)){
+    print("OK".PHP_EOL);
+}else{
+    print("NG".PHP_EOL);
+}
 
-// echo PHP_EOL;
+if(array_key_exists("age",$data2)){
+    print("OK".PHP_EOL);
+}else{
+    print("NG".PHP_EOL);
+}
 
-// print("#####q16#####".PHP_EOL);
-// $users = [
-//     ["name" => "satou", "age" => 22],
-//     ["name" => "yamada", "age" => 12],
-//     ["name" => "takahashi", "age" => 32],
-//     ["name" => "nakamura", "age" => 41]
-// ];
+echo PHP_EOL;
 
-// // 以下に回答を記載
+print("#####q16#####".PHP_EOL);
+$users = [
+    ["name" => "satou", "age" => 22],
+    ["name" => "yamada", "age" => 12],
+    ["name" => "takahashi", "age" => 32],
+    ["name" => "nakamura", "age" => 41]
+];
 
-// echo PHP_EOL;
+// 以下に回答を記載
+foreach($users as $user){
+    echo "私の名前は".$user["name"]."です。年齢は".$user["age"]."歳です".PHP_EOL;
+}
+
+echo PHP_EOL;
 
 // print("#####q17#####".PHP_EOL);
 // class User
