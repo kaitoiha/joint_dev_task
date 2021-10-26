8effe0dedcc88537d9e7b0167e6e5dace0ab6d45
kaitoiha <kaitooooo18@gmail.com>
2021-10-26 15:25:36 +0900

    q17-q19

--- task.php
+++ task.php
@@ -199,50 +199,81 @@ foreach($users as $user){
 
 echo PHP_EOL;
 
-// print("#####q17#####".PHP_EOL);
-// class User
-// {
+print("#####q17#####".PHP_EOL);
+class User
+{
+    // コードを追加
+    private $name;
+    private $age;
+    private $gender;
 
-//     // コードを追加
+    function __construct($name, $age, $gender)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+    }
+    function info(){
+        print("名前:" . $this->name . PHP_EOL);
+        print("年齢:" . $this->age . PHP_EOL);
+        print("性別:" . $this->gender . PHP_EOL);
+    }
+}
 
-// }
+$user1 = new User("神里", 32, "男");
+$user2 = new User("あじー", 32, "男");
 
-// $user1 = new User("神里", 32, "男");
-// $user2 = new User("あじー", 32, "男");
+$user1->info();
+print("-------------".PHP_EOL);
+$user2->info();
 
-// $user1->info();
-// print("-------------".PHP_EOL);
-// $user2->info();
+echo PHP_EOL;
 
-// echo PHP_EOL;
+print("#####q18#####".PHP_EOL);
 
-// print("#####q18#####".PHP_EOL);
+// コードを追加
+class Man
+{
+    private $name;
+    private $age;
 
-// // コードを追加
+    function __construct($name,$age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+    function introduce(){
+        if ($this->age >= 20){
+            print("こんにちは," . $this->name . "と申します。宜しくお願いいたします。" . PHP_EOL);
+        }else{
+            print("はいさいまいど〜、" . $this->name . "です！！！" . PHP_EOL);
+        }
+    }
+}
 
-// $man1 = new Man("あじー", 32);
-// $man2 = new Man("ゆたぼん", 10);
+$man1 = new Man("あじー", 32);
+$man2 = new Man("ゆたぼん", 10);
 
-// $man1->introduce();
-// $man2->introduce();
+$man1->introduce();
+$man2->introduce();
 
-// echo PHP_EOL;
+echo PHP_EOL;
 
-// print("#####q19#####".PHP_EOL);
-// class Item
-// {
-//     // 以下を修正して下さい
+print("#####q19#####".PHP_EOL);
+class Item
+{
+    // 以下を修正して下さい
 
-//     protected $name;
+    public $name;
 
-//     function __construct($book_name){
-//         $this->name = $book_name;
-//     }
-// }
-// // 以下は変更しないで下さい
+    function __construct($book_name){
+        $this->name = $book_name;
+    }
+}
+// 以下は変更しないで下さい
 
-// $book = new Item("ゼロ秒思考");
-// print($book->name.PHP_EOL);
+$book = new Item("ゼロ秒思考");
+print($book->name.PHP_EOL);
 
 // echo PHP_EOL;
 
